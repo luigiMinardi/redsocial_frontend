a78d06270762e666d9d16a442ca37479a404d08f
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-03-29 21:03:18 +0200

    he hecho la plantilla de la pagina sobre nosotros

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './Containers/Home/Home';
 import Registrar from './Containers/Registrar/Registrar';
+import Nosotros from './Containers/Nosotros/Nosotros';
 
 
 function App() {
@@ -10,10 +11,11 @@ function App() {
     <div className="App">
 
       <BrowserRouter>
-        
+
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/registrar" element={<Registrar/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/registrar" element={<Registrar />} />
+          <Route path="/nosotros" element={<Nosotros />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Margin/Margin.jsx
+++ src/Components/Margin/Margin.jsx
@@ -3,23 +3,23 @@ import { useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import 'antd/dist/antd.css';
-import {Input,Button} from 'antd';
+import { Input, Button } from 'antd';
 import './Margin.css';
 
 const Margin = (props) => {
-    console.log ("estamos como admin")
+    console.log("estamos como admin")
     const navigate = useNavigate()
     return (
         <div className='designMargin'>
-            <a onClick={()=> navigate('/')}>Home</a>    
-            <a onClick={()=> navigate ('/registrar')}>Registrar</a>    
-            <a>Usuario/Iniciar Session</a>    
-            <a onClick={()=> navigate ('/sobre-nosotros')}>Sobre Nosotros</a>    
-            <a>Cerrar Session</a>    
+            <a onClick={() => navigate('/')}>Home</a>
+            <a onClick={() => navigate('/registrar')}>Registrar</a>
+            <a>Usuario/Iniciar Session</a>
+            <a onClick={() => navigate('/nosotros')}>Sobre Nosotros</a>
+            <a>Cerrar Session</a>
         </div>
     )
 
-} 
+}
 
 
 export default Margin;
