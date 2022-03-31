fe3cc484df0e8e5083f9d6ca399fecad05d3a17d
RobertoGuzman <robertoguzman64@gmail.com>
2022-03-31 17:38:20 +0200

    feat: Mejorado el diseño del Margin

--- src/Components/Margin/Margin.jsx
+++ src/Components/Margin/Margin.jsx
@@ -11,11 +11,11 @@ const Margin = (props) => {
     const navigate = useNavigate()
     return (
         <div className='designMargin'>
-            <a onClick={() => navigate('/')}>Home</a>
-            <a onClick={() => navigate('/registrar')}>Registrar</a>
-            <a onClick={() => navigate('/login')}>Usuario/Iniciar Session</a>
-            <a onClick={() => navigate('/nosotros')}>Sobre Nosotros</a>
-            <a>Cerrar Session</a>
+            <a className='boton' onClick={() => navigate('/')}>Home</a>
+            <a className='boton' onClick={() => navigate('/registrar')}>Registrar</a>
+            <a className='boton' onClick={() => navigate('/login')}>Usuario/Iniciar Session</a>
+            <a className='boton' onClick={() => navigate('/nosotros')}>Sobre Nosotros</a>
+            <a className='boton' >Cerrar Session</a>
         </div>
     )
 
