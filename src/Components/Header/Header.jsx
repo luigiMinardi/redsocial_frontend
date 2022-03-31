b82cb5a5fd240a7a9f72b27bddc07519d5d922e1
RobertoGuzman <robertoguzman64@gmail.com>
2022-03-31 19:29:25 +0200

    feat: Añadidos dos nuevos botones de navegación al estar Logueado el Usuario en Margin.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,24 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
-import axios from 'axios';
-// import 'antd/dist/antd.css';
-// import {Input,Button} from 'antd';
-
 import './Header.css';
 
+
 const Header = (props) => {
-console.log ("estamos como admin")
-        return (
-            <div className='designHeader'> Logo
-                   
-            </div>
-        )
-
-    } 
+    return (
+        <div className='designHeader'>{props.credenciales?.usuario.nombre}{props.credenciales?.usuario.apellidos}
+        </div>
+    )
+}
 
 
-export default Header;
-// export default connect (Header);
-
-//..
+export default connect((state) => ({
+    credenciales: state.credenciales
+}))(Header);
--- src/Components/Margin/Margin.jsx
+++ src/Components/Margin/Margin.jsx
@@ -1,24 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { connect } from 'react-redux';
-import axios from 'axios';
-
 import './Margin.css';
+// Redux
+import { connect } from 'react-redux';
+import { LOGOUT } from '../../redux/types';
+
 
 const Margin = (props) => {
-    console.log("estamos como admin")
+
     const navigate = useNavigate()
+    const logOut = () => {
+        //Borrar de RDX las credenciales
+        props.dispatch({ type: LOGOUT });
+        setTimeout(() => {
+            navigate("/");
+        }, 500);
+    }
+
+    if (props.credenciales.token === '') {
     return (
         <div className='designMargin'>
             <a className='boton' onClick={() => navigate('/')}>Home</a>
             <a className='boton' onClick={() => navigate('/registrar')}>Registrarse</a>
             <a className='boton' onClick={() => navigate('/login')}>Login</a>
             <a className='boton' onClick={() => navigate('/nosotros')}>Sobre Nosotros</a>
-            <a className='boton' >Cerrar Session</a>
+            <a className='boton' onClick={() => logOut()}>Cerrar Session</a>
         </div>
     )
-
+    } else {
+        return(
+            <div className='designMargin'>
+            <a className='boton' onClick={() => navigate('/')}>Home</a>
+            <a className='boton' onClick={() => navigate('/publicaciones')}>Publicaciones</a>
+            <a className='boton' onClick={() => navigate('/perfil')}>Perfil</a>
+            <a className='boton' onClick={() => navigate('/nosotros')}>Sobre Nosotros</a>
+            <a className='boton' onClick={() => logOut()}>Cerrar Session</a>
+        </div>
+        )
+    }
 }
 
 
-export default Margin;
+export default connect((state) => ({
+    credenciales: state.credenciales
+}))(Margin);
