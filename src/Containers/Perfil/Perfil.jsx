598440ae55ab4997670eeec13eb046e4c8041251
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-04-02 18:31:56 +0200

    añadido los enlaces a los butones en la pagina de perfil

--- src/Containers/Perfil/Perfil.jsx
+++ src/Containers/Perfil/Perfil.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useHref, useNavigate } from 'react-router-dom';
 // axios
 import axios from 'axios';
 import { baseURL } from '../../utiles';
@@ -25,13 +25,9 @@ const Perfil = (props) => {
         ciudad: props.credenciales.usuario.ciudad,
     })
 
-    // Handler (Manejador).
-    const rellenarDatos = (e) => {
-        setDatosUsuario({
-            ...datosUsuario,
-            [e.target.name]: e.target.value
-        })
-    };
+    useEffect(() => {
+        muestraUsuario();
+    }, [])
 
     useEffect(() => {
         if (props.credenciales.token === '') {
@@ -39,60 +35,83 @@ const Perfil = (props) => {
         };
     })
 
-    const actualizaUsuario = async () => {
-        let body = {
-            id: props.credenciales.usuario.id,
-            nombre: datosUsuario.nombre,
-            edad: datosUsuario.edad,
-            apellidos: datosUsuario.apellidos,
-            telefono: datosUsuario.telefono,
-            ciudad: datosUsuario.ciudad,
-        }
-        console.log("papayote", body)
+    const muestraUsuario = async () => {
         let config = {
             headers: { Authorization: `Bearer ${props.credenciales.token}` }
         };
         try {
-            // Actualizamos los datos de Usuario en nuestra base de datos.
-            let res = await axios.put(`${baseURL}usuarios/${props.credenciales.usuario.id}`, body, config);
-            if (res) {
-                // Guardamos los datos en Redux.
-                props.dispatch({ type: MODIFICAR_CREDENCIALES, payload: datosUsuario });
-            }
+            let res = await axios.get(`${baseURL}usuarios/${props.credenciales.usuario.id}`, config);
+            setDatosUsuario(res.data)
         } catch (error) {
             console.log(error)
         }
     }
 
-    return (
-        <div className='designPerfil2'>
-            <Header />
-            <div className="contenidoPerfil2">
-                <Margin />
-                <div className='cuerpoPerfil2'>
-                    <div className='usuarioCaja'>
-                        <div className="sobreYo">
-                            <div className="nombreFila">Callum Joseph Iain Gordon</div>
-                            <div className="contenidoFila">
-                                <div className="imagenUsuario"></div>
-                                <div className="edadCiudad">
-                                    <div className="ciudadUsuario">Ciudad:</div>
-                                    <div className="edadUsuario">Edad:</div>
+    if (datosUsuario.nombre !== '') {
+        return (
+            <div className='designPerfil2'>
+                <Header />
+                <div className="contenidoPerfil2">
+                    <Margin />
+                    <div className='cuerpoPerfil2'>
+                        <div className='usuarioCaja'>
+                            <div className="sobreYo">
+                                <div className="nombreFila">{datosUsuario.nombre}</div>
+                                <div className="contenidoFila">
+                                    <div className="imagenUsuario"></div>
+                                    <div className="edadCiudad">
+                                        <div className="ciudadUsuario">{datosUsuario.ciudad}</div>
+                                        <div className="edadUsuario">{datosUsuario.edad}</div>
+                                    </div>
                                 </div>
+                                <div className="informacionUsuario">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</div>
+                                <div className="butonesFondo">
+                                    <div className="botonPerfil"onClick={() => navigate('/editar-perfil')}> Editar Perfil</div>
+                                    <div className="botonSeguir"> Seguir!</div>
+                                </div>
+
                             </div>
-                            <div className="informacionUsuario">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</div>
-                            <div className="butonSeguir"> Seguirle!</div>   
-                        </div>
-                        <div className="torreDatos">
-                            <div className="posts">Posts: </div>
-                            <div className="siguiendo">Siguiendo: </div>
-                            <div className="seguidores">Seguidores: </div>
+                            <div className="torreDatos">
+                                <div className="posts">Posts: </div>
+                                <div className="siguiendo" onClick={() => navigate('/siguiendo')}>Siguiendo: </div>
+                                <div className="seguidores" onClick={() => navigate('/seguidores')}>Seguidores: </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className='designPerfil2'>
+                <Header />
+                <div className="contenidoPerfil2">
+                    <Margin />
+                    <div className='cuerpoPerfil2'>
+                        <div className='usuarioCaja'>
+                            <div className="sobreYo">
+                                <div className="nombreFila">Callum Joseph Iain Gordon</div>
+                                <div className="contenidoFila">
+                                    <div className="imagenUsuario"></div>
+                                    <div className="edadCiudad">
+                                        <div className="ciudadUsuario">Ciudad:</div>
+                                        <div className="edadUsuario">Edad:</div>
+                                    </div>
+                                </div>
+                                <div className="informacionUsuario">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</div>
+                            </div>
+                            <div className="torreDatos">
+                                <div className="posts">Posts: </div>
+                                <div className="siguiendo">Siguiendo: </div>
+                                <div className="seguidores">Seguidores: </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
 }
 export default connect((state) => ({
     credenciales: state.credenciales
