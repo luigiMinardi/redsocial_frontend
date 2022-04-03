d6d6f150c2727375d4902cc720e90aec2fd9060d
Luigi Minardi <lugminsinlag@gmail.com>
2022-04-03 23:44:29 +0200

    feat(hilo): Arreglando la funcionalidad del hilo, haciendo con que el boton de editar aparezca solo para el dueño del hilo.

--- src/Containers/Hilo/Hilo.jsx
+++ src/Containers/Hilo/Hilo.jsx
@@ -9,35 +9,67 @@ import Header from '../../Components/Header/Header';
 
 const Hilo = (props) => {
     let navigate = useNavigate();
+    const [datosHilo, setDatosHilo] = useState({});
+
     useEffect(() => {
-        // Compruebo si hay datos de la publicacion escogida en redux, en caso de NO
-        // haber datos, redirijo a HOME.
-        if (props.datosHilo?.publicacion === undefined) {
-            navigate("/hilo");
+        muestraHilo();
+    }, []);
+
+    const muestraHilo = async () => {
+        let config = {
+            headers: { Authorization: `Bearer ${props.credenciales.token}` }
+        };
+        try {
+            let res = await axios.get(`${baseURL}/hilos/${props.datosHilo._id}`, config);
+            setDatosHilo(res.data)
+        } catch (error) {
+            console.log(error)
         }
-    },[]);
-    return (
-        <div className='paginaHilo'>
-            <Header />
-            <div className="contenidoHilo">
-                <Margin />
-                <div className='cuerpoHilo'>
-                    <div className="foroPostHilo">
-                        <div className="postCabezaHilo"></div>
-                        <div className='contenidoPostHilo'>
-                            <p className='tituloHiloHome'>{props.datosHilo.titulo}</p>
-                            <p className='cuerpoHiloHome'>{props.datosHilo.cuerpo}</p>
-                            <div className="botonHilo">
-                                Modificar Hilo
+    }
+
+    if (Object.entries(datosHilo).length !== 0) {
+        return (
+            <div className='paginaHilo'>
+                <Header />
+                <div className="contenidoHilo">
+                    <Margin />
+                    <div className='cuerpoHilo'>
+                        <div className="foroPostHilo">
+                            <div className="postCabezaHilo"></div>
+                            <div className='contenidoPostHilo'>
+                                <p className='tituloHiloHome'>{datosHilo.titulo}</p>
+                                <p className='cuerpoHiloHome'>{datosHilo.cuerpo}</p>
+                                {datosHilo?.usuario.usuarioId === props.credenciales.usuario._id
+                                    && <div className="botonHilo" onClick={() => navigate('/editar-hilo')}>
+                                        Modificar Hilo
+                                    </div>
+                                }
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div >
-    );
+            </div >
+        );
+    } else {
+        return (
+            <div className='paginaHilo'>
+                <Header />
+                <div className="contenidoHilo">
+                    <Margin />
+                    <div className='cuerpoHilo'>
+                        <div className="foroPostHilo">
+                            <div className="postCabezaHilo"></div>
+                            <div className='contenidoPostHilo'>
+                                <p className='tituloHiloHome'>404 hilo no encontrado</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        );
+    }
 }
 export default connect((state) => ({
     credenciales: state.credenciales,
-    datosHilo: state.datosHilo.publicacion
+    datosHilo: state.datosHilo
 }))(Hilo);
