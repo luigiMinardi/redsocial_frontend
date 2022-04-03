2b656aa54a7f3ca652672ea30ef2215d71bae25c
Luigi Minardi <lugminsinlag@gmail.com>
2022-04-03 19:04:26 +0200

    fix: Arreglando la responsividad de Margin y Reader. Borrando el App.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Containers/Home/Home';
 import Registrar from './Containers/Registrar/Registrar';
